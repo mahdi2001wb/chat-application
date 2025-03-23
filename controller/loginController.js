d238a3bbeb331eafa43ae66144b494ad1c62b54e
Imam Mahdi <mahsiimam0@gmail.com>
2025-03-24 00:42:02 +0600

    bcryptjs add

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,5 +1,5 @@
 // External Imports
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const createError = require("http-errors");
 
--- controller/usersController.js
+++ controller/usersController.js
@@ -1,5 +1,5 @@
 // External imports
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { unlink } = require("fs");
 const path = require("path");
 
